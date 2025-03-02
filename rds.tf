bfea6fdfdc18ef63c057169265316d202fbd9ef7
Amine Mastouri <amin.mastouri01@gmail.com>
2025-03-02 03:35:07 +0100

    updating rds.tf

--- rds.tf
+++ rds.tf
@@ -11,14 +11,17 @@ resource "aws_db_instance" "rds" {
   multi_az               = true
   db_subnet_group_name   = aws_db_subnet_group.my_db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rds-sg.id]
+  depends_on = [ aws_db_instance.rds ]
+
 }
 
 resource "aws_db_subnet_group" "my_db_subnet_group" {
   name       = "my-db-subnet-group"
   subnet_ids = [aws_subnet.private-subnet-rds-az1.id, aws_subnet.private-subnet-rds-az2.id] #AWS will create automatically the rds in one of these two subnets , and its standBy in the other subnet 
-
+  description = "subnet for rds"
   tags = {
     Name = "My RDS Subnet Group"
   }
   
+
 }
